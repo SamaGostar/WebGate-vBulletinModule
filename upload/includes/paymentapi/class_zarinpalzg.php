b44544ad22d47a54878a1cda36a820e3b63fc7a4
Masoud A <amini141@gmail.com>
2016-06-14 11:02:24 +0430

    add zaringate

--- upload/includes/paymentapi/class_zarinpalzg.php
+++ upload/includes/paymentapi/class_zarinpalzg.php
@@ -1,4 +1,5 @@
 <?php
+
 /*=============================================================================================*\
 || ########################################################################################### ||
 || # Product Name	: Zarin Pal Payment API Module for vBulletin		Version: 4.X.X
@@ -18,7 +19,7 @@ if (!isset($GLOBALS['vbulletin']->db))
 * @version	$Revision: 20000 $
 * @date		$Date: 2012-03-25 01:24:45 +0350 (Sun, 25 March 2012) $
 */
-class vB_PaidSubscriptionMethod_zarinpalwg extends vB_PaidSubscriptionMethod
+class vB_PaidSubscriptionMethod_zarinpalzg extends vB_PaidSubscriptionMethod
 {
 	var $supports_recurring = false;	 
 	var $display_feedback = true;
@@ -26,9 +27,9 @@ class vB_PaidSubscriptionMethod_zarinpalwg extends vB_PaidSubscriptionMethod
 	function verify_payment()
 	{		
 		$this->registry->input->clean_array_gpc('r', array(
-			'item'		=> TYPE_STR,			
-			'Authority'	=> TYPE_STR,
-			'Status'	=> TYPE_STR
+			'item'    => TYPE_STR,			
+			'Authority'    => TYPE_STR,
+                        'Status'    => TYPE_STR
 		));  
 		
 		if (!class_exists('SoapClient'))
@@ -36,24 +37,30 @@ class vB_PaidSubscriptionMethod_zarinpalwg extends vB_PaidSubscriptionMethod
 			$this->error = 'SOAP is not installed';
 			return false;
 		}
-		
 		if (!$this->test())
 		{
 			$this->error = 'Payment processor not configured';
 			return false;
 		}
-		
 		$this->transaction_id = $this->registry->GPC['Authority'];
-		if(!empty($this->registry->GPC['item']) AND !empty($this->registry->GPC['Authority']))
+
+		if(!empty($this->registry->GPC['Authority']))
 		{
+
+
+
 			$this->paymentinfo = $this->registry->db->query_first("
 				SELECT paymentinfo.*, user.username
 				FROM " . TABLE_PREFIX . "paymentinfo AS paymentinfo
 				INNER JOIN " . TABLE_PREFIX . "user AS user USING (userid)
 				WHERE hash = '" . $this->registry->db->escape_string($this->registry->GPC['item']) . "'
 			");
-			if (!empty($this->paymentinfo) && $this->registry->GPC['Status'] == 'OK')
+			if (!empty($this->paymentinfo) && $this->registry->GPC['Status'] == "OK")
 			{
+
+
+
+
 				$sub = $this->registry->db->query_first("SELECT * FROM " . TABLE_PREFIX . "subscription WHERE subscriptionid = " . $this->paymentinfo['subscriptionid']);
 				$cost = unserialize($sub['cost']);				
 				$amount = floor($cost[0][cost][usd]*$this->settings['d2t']);
@@ -61,9 +68,9 @@ class vB_PaidSubscriptionMethod_zarinpalwg extends vB_PaidSubscriptionMethod
 				$client = new SoapClient('https://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding'=>'UTF-8'));
 				$res = $client->PaymentVerification(
 					array(
-						'MerchantID'	 => $this->settings['zpmid'],
-						'Authority' 	 => $this->registry->GPC['Authority'],
-						'Amount'	 => $amount
+						'MerchantID'	 => $this->settings['zpmid'] ,
+						'Authority' 	 => $this->registry->GPC['Authority'] ,
+						'Amount'	 	=> $amount
 					));	
 				if($res->Status == 100)
 				{
@@ -71,15 +78,13 @@ class vB_PaidSubscriptionMethod_zarinpalwg extends vB_PaidSubscriptionMethod
 					$this->paymentinfo['amount'] = $cost[0][cost][usd];				
 					$this->type = 1;								
 					return true;					
-				} else {
-					$this->error = 'ERR: '. $res->Status;
-					return false;
-				}				
-			} else {
-				$this->error = 'Invalid trasaction';
-				return false;	
+				}else{
+					echo'ERR: '.$res->Status;
+				}
+				
 			}
-		}else{		
+		}
+		else{
 			$this->error = 'Duplicate transaction.';
 			return false;
 		}
@@ -89,6 +94,7 @@ class vB_PaidSubscriptionMethod_zarinpalwg extends vB_PaidSubscriptionMethod
 	{	
 		if (class_exists('SoapClient')){
 			if(!empty($this->settings['zpmid']) AND !empty($this->settings['d2t'])){
+				$client = new SoapClient('https://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8'));
 				return true;
 			}
 		}
@@ -103,18 +109,18 @@ class vB_PaidSubscriptionMethod_zarinpalwg extends vB_PaidSubscriptionMethod
 		$cost = floor($cost*$this->settings['d2t']);		
 		$merchantID = $this->settings['zpmid'];
 		
-		$form['action'] = 'zarinpalwg.php';
+		$form['action'] = 'zarinpalzg.php';
 		$form['method'] = 'POST';        
 			
 		$settings =& $this->settings;
 		
-		$templater = vB_Template::create('subscription_payment_zarinpalwg');
+		$templater = vB_Template::create('subscription_payment_zarinpalzg');
 	     	$templater->register('merchantID', $merchantID);
-		$templater->register('cost', $cost);
-		$templater->register('item', $item);					
-		$templater->register('subinfo', $subinfo);
-		$templater->register('settings', $settings);
-		$templater->register('userinfo', $userinfo);
+			$templater->register('cost', $cost);
+			$templater->register('item', $item);					
+			$templater->register('subinfo', $subinfo);
+			$templater->register('settings', $settings);
+			$templater->register('userinfo', $userinfo);
 		$form['hiddenfields'] .= $templater->render();
 		return $form;
 	}
