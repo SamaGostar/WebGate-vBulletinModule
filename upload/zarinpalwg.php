bef6e6d5df629cab48618a9f5a6adfbc9e57ec68
aliafi <alireza.yousefi98@gmail.com>
2021-04-08 19:04:27 +0430

    update v4
    update v4

--- upload/zarinpalwg.php
+++ upload/zarinpalwg.php
@@ -24,18 +24,11 @@ $result = json_decode($result, true, JSON_PRETTY_PRINT);
 curl_close($ch);
 
 if ($result['data']['code'] == 100) {
-    echo' <html><body>
-                    <script type="text/javascript" src="https://cdn.zarinpal.com/zarinak/v1/checkout.js"></script>
-                    <script type="text/javascript">
-                    window.onload = function () {
-                    Zarinak.setAuthority("' . $result['data']['authority'] . '");
-                    Zarinak.showQR();
-                    Zarinak.open();
-                 };
-                </script></body></html>';
+
+    header('Location: https://www.zarinpal.com/pg/StartPay/' . $result['data']["authority"]);
 
 } else {
-    echo 'ERR: ' . $result['errors']['code'];
+    echo 'ERR: ' .  $result['errors']['code'];
 }
 
 ?>
